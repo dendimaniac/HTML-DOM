60de8f880b1df7ab55350dd4c291e66e66575923
Dendi Maniac <daq30899@gmail.com>
2019-10-24 14:21:15 +0300

    Finished

--- js/main-b.js
+++ js/main-b.js
@@ -1,1 +1,13 @@
-// Put code of task B here
+'use strict';
+const main = document.querySelector('main');
+
+main.innerHTML += `<article>
+            <header>
+                <h2>Other header</h2>
+            </header>
+            <figure>
+                <img src="http://placekitten.com/320/162" alt="title">
+                <figcaption>Some other cat</figcaption>
+            </figure>
+            <p>Here is some text. Here is some text. Here is some text. Here is some text.</p>
+        </article>`;
--- js/main-c.js
+++ js/main-c.js
@@ -1,1 +1,29 @@
-// Put code of task C here
+'use strict';
+const main = document.querySelector('main');
+
+const image = document.createElement('img');
+image.src = "http://placekitten.com/320/162";
+image.alt = "some cat";
+
+const figCaption = document.createElement('figcaption');
+figCaption.innerHTML = 'Some caption';
+
+const figure = document.createElement('figure');
+figure.appendChild(image);
+figure.appendChild(figCaption);
+
+const h2 = document.createElement('h2');
+h2.innerHTML = 'Other header';
+
+const header = document.createElement('header');
+header.appendChild(h2);
+
+const p = document.createElement('p');
+p.innerHTML = 'Here is some text. Here is some text. Here is some text. Here is some text.';
+
+const article = document.createElement('article');
+article.appendChild(header);
+article.appendChild(figure);
+article.appendChild(p);
+
+main.appendChild(article);
